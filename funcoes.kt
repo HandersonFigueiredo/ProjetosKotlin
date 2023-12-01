97dcfb4470557102c3df994642cbc0dc7a5ad998
HandersonFigueiredo <hfigueiredo.cc@gmail.com>
2023-12-01 18:52:07 -0400

    Primeiro Codigo Kotlin

---
+++ funcoes.kt
@@ -0,0 +1,21 @@
+fun printMessage(message: String): Unit {
+    println(message)
+}
+
+fun printMessageWhitPrefix(message: String, prefix: String = "Atenção!"){
+    println("<<$prefix>>, $message")
+}
+
+fun soma(x: Int, y: Int): Int {
+    return x + y
+}
+
+fun mutiply(x: Int, y: Int) = x * y
+
+fun main(){
+    printMessage("Ola Mundão")
+    printMessageWhitPrefix("Isso é um Teste de Função.")
+    println("O Resultado da SOMA é = " + soma (5, 25))
+    println("0 Resultado da MULTIPLICAÇÃO é = " + mutiply(5, 10))
+
+}
