7ebf4932c23e4c3e8a291fc526119d2b98be881a
HandersonFigueiredo <hfigueiredo.cc@gmail.com>
2023-12-01 19:01:06 -0400

    Primeiro Codigo Kotlin

--- funcoes.kt
+++ funcoes.kt
@@ -15,6 +15,9 @@ fun mutiply(x: Int, y: Int) = x * y
 fun main(){
     printMessage("Ola Mundão")
     printMessageWhitPrefix("Isso é um Teste de Função.")
+    printMessageWhitPrefix("Hello", "Log")
+    printMessageWhitPrefix("Hello")
+    printMessageWhitPrefix(prefix = "Log", message = "Hello")
     println("O Resultado da SOMA é = " + soma (5, 25))
     println("0 Resultado da MULTIPLICAÇÃO é = " + mutiply(5, 10))
 
