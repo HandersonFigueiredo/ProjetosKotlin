757f6b8d6b1f487440ff233191bb383e50d81dd9
HandersonFigueiredo <hfigueiredo.cc@gmail.com>
2023-12-05 17:33:33 -0400

    codando em  Kotlin

---
+++ loops1.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+    val cakes = listOf("cenoura", "queijo", "chocolate")
+    
+    for (cake in cakes) {                               // 1
+        println("Hummm!, é um bolo de $cake!")
+    }
+
+
+}
